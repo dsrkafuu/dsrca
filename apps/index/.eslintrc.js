32320796028282fea8b75cd1f3a85c51d4e01e03
dsrkafuu <dsrkafuu@outlook.com>
2022-11-05 15:20:22 +0800

    repo: refresh repo

--- apps/index/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-const { getESLintConfig } = require('@dsrca/config');
-
-module.exports = getESLintConfig('next', {
-  settings: {
-    next: {
-      rootDir: './apps/*/',
-    },
-  },
-});
--- packages/design/src/lib/lodash.ts
+++
@@ -1,3 +0,0 @@
-export function dod() {
-  return true;
-}
