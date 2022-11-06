9dcabce4262f3481dde90e1ffaf03528848d1562
dsrkafuu <dsrkafuu@outlook.com>
2022-11-06 09:39:05 +0800

    feat: google analytics

--- apps/index/pages/_app.tsx
+++ apps/index/pages/_app.tsx
@@ -2,12 +2,14 @@ import '@dsrca/react/es/index.css';
 import '../styles/global.scss';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 import SakanaWidget from '../components/SakanaWidget';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <GoogleAnalytics trackPageViews />
       <SakanaWidget />
     </>
   );
