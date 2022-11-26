f9cbd35c4b3ade3bb57c01a09de5412662a77623
dsrkafuu <dsrkafuu@outlook.com>
2022-11-26 10:33:21 +0800

    fix: jsx runtime

--- packages/config/eslint/next.js
+++ packages/config/eslint/next.js
@@ -12,6 +12,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:@next/next/recommended',
     'prettier',
