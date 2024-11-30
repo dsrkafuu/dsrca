9cdbfd0bb32a56cb5ff03dd3a20342f9288b9685
dsrkafuu <dsrkafuu@outlook.com>
2024-11-30 15:28:45 +0800

    feat: update assets & next config

---
+++ packages/config/eslint/next.config.js
@@ -0,0 +1,43 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import next from '@next/eslint-plugin-next';
+
+export default [
+  {
+    languageOptions: {
+      globals: { ...globals.node, ...globals.browser },
+    },
+  },
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  {
+    ...react.configs.flat.recommended,
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  react.configs.flat['jsx-runtime'],
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+  {
+    plugins: {
+      'next': next,
+    },
+    rules: {
+      ...next.configs.recommended.rules,
+    },
+  },
+  prettier,
+];
